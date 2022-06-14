c50d894168e17867fab4f63b0c8e8d63ce8d2b1b
Nitish <66010219+Nitishz@users.noreply.github.com>
2022-06-14 10:30:59 +0530

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRef, useState } from 'react'
+import { AiOutlineShoppingCart, AiFillCloseCircle, AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai'
+import { BsFillBagCheckFill } from 'react-icons/bs'
+import { MdAccountCircle } from 'react-icons/md'
+
+
+const Navbar = ({user, cart, addToCart, removeFromCart, clearCart, subTotal, logout}) => {
+
+    const [dropdown, setDropdown] = useState(false)
+
+
+    const toggleCart = () => {
+        if (ref.current.classList.contains('translate-x-full')) {
+            ref.current.classList.remove('translate-x-full')
+            ref.current.classList.add('translate-x-0')
+        }
+        else if (!ref.current.classList.contains('translate-x-full')) {
+            ref.current.classList.remove('translate-x-0')
+            ref.current.classList.add('translate-x-full')
+        }
+    }
+
+    const ref = useRef()
+
+
+    return (
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md sticky top-0 bg-white z-10'>
+        <div className="logo mr-auto md:mx-5">
+            <Link href={'/'}><a><Image src='/footer.png' alt='' width={200} height={40}/></a></Link>
+        </div>
+        <div className="nav">
+            <ul className='flex items-center space-x-4 font-bold md:text-md'>
+                <Link href={'/tshirts'}><a><li className='hover:text-indigo-500'>Tshirts</li></a></Link>
+                <Link href={'/hoodies'}><a><li className='hover:text-indigo-500'>Hoodies</li></a></Link>
+                <Link href={'/stickers'}><a><li className='hover:text-indigo-500'>Stickers</li></a></Link>
+                <Link href={'/mugs'}><a><li className='hover:text-indigo-500'>Mugs</li></a></Link>
+            </ul>
+        </div>
+        <div className="cart items-center absolute right-0 top-4 mx-5 cursor-pointer flex">
+            <span onMouseOver={() => {setDropdown(true)}} onMouseLeave={() => {setDropdown(false)}}>
+                {dropdown && <div onMouseOver={() => {setDropdown(true)}} onMouseLeave={() => {setDropdown(false)}} className="absolute right-8 bg-white shadow-lg border top-5 py-4 rounded-md px-5 w-32">
+                    <ul>
+                        <Link href={'/myaccount'}><a><li className='py-1 hover:text-indigo-700 text-sm'>My Account</li></a></Link>
+                        <Link href={'/orders'}><a><li className='py-1 hover:text-indigo-700 text-sm'>Orders</li></a></Link>
+                        <li onClick={logout} className='py-1 hover:text-indigo-700 text-sm'>Logout</li>
+                    </ul>
+                </div>}
+                {user.value && <MdAccountCircle className='text-xl md:text-2xl mx-2' />}
+            </span>
+
+            {!user.value && <Link href={'/login'}>
+                <a><button className='bg-indigo-600 px-2 py-1 rounded-md text-sm text-white mx-2'>Login</button></a>
+            </Link>}
+            <AiOutlineShoppingCart onClick={toggleCart} className='text-xl md:text-2xl'/>
+        </div>
+
+        
+        <div ref={ref} className={`w-72 h-[100vh] sidecart overflow-y-scroll absolute top-0 right-0 bg-indigo-100 px-8 py-10 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-0' : 'translate-x-full'}`}>
+            <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
+            <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl"><AiFillCloseCircle className='text-indigo-500'/></span>
+            <ol className='list-decimal font-semibold'>
+                {Object.keys(cart).length === 0 && <div className='my-4 font-semibold'>Cart is Empty</div>}
+                {Object.keys(cart).map((k)=> { return (<li key={k}>
+                    <div className="item flex my-5">
+                        <div className='w-2/3 font-semibold'>{cart[k].name} ({cart[k].size}/{cart[k].variant})</div>
+                        <div className='flex items-center justify-center w-1/3 font-semibold text-lg'><AiFillMinusCircle onClick={() => {removeFromCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)}} className='cursor-pointer text-indigo-500' /><span className='mx-2 text-sm'>{cart[k].qty}</span><AiFillPlusCircle onClick={() => {addToCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)}} className='cursor-pointer text-indigo-500' /></div>
+                    </div>
+                </li>)})}
+            </ol>
+            <div className='font-bold mb-2'>Total: ₹{subTotal}</div>
+            <div className="flex">
+                <Link href={'/checkout'}><button className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm font-semibold"><BsFillBagCheckFill className='m-1' /> Checkout</button></Link>
+                <button onClick={clearCart} className="flex mr-2 text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm font-semibold">Clear Cart</button>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Navbar
